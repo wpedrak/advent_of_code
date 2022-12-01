2a9f7b158b22ceac056f538e0fec40a8408055b2
Wojciech Pedrak <woj123@gmail.com>
2022-12-01 12:14:45 +0100

    2022-01 done

---
+++ 2022/01/solution2.py
@@ -0,0 +1,13 @@
+elves = []
+elf = 0
+for line in open('input.txt', 'r', encoding='utf-8'):
+    line = line.rstrip()
+    if not line:
+        elves.append(elf)
+        elf = 0
+        continue
+
+    elf += int(line)
+
+elves.append(elf)
+print(sum(sorted(elves)[-3:]))
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 My solutions to [advent of code](https://adventofcode.com/)
 
+* [2022](https://adventofcode.com/2022)
 * [2021](https://adventofcode.com/2021)
 * [2020](https://adventofcode.com/2020)
 * [2019](https://adventofcode.com/2019)

7014c4507075ad4c6f3a25aa123255925b725454
Wojciech Pedrak <woj123@gmail.com>
2022-12-11 20:03:45 +0100

    2022-11 done

---
+++ 2022/11/solution2.py
@@ -0,0 +1,70 @@
+import functools
+from collections import deque
+
+class Monkey:
+    def __init__(self, items: list[int], operation, divisor: int, target_true: int, target_false: int):
+        self.items = deque(items)
+        self.operation = operation
+        self.divisor = divisor
+        self.target_true = target_true
+        self.target_false = target_false
+        self.inspections = 0
+        self.worry_level_limiter = 1
+
+    def inspect(self) -> tuple[int, int]:
+        self.inspections += 1
+        item = self.items.popleft()
+        new_item = self.operation(item) % self.worry_level_limiter
+        target = self.target_true if new_item % self.divisor == 0 else self.target_false
+        return new_item, target
+
+    def receive(self, item) -> None:
+        self.items.append(item)
+
+    def inspections_number(self) -> int:
+        return self.inspections
+
+    def set_worry_level_limiter(self, worry_level_limiter) -> None:
+        self.worry_level_limiter = worry_level_limiter
+
+def read_lines(file_name: str = 'input.txt') -> list[str]:
+    return [line.rstrip() for line in open(file_name, 'r', encoding='utf-8')]
+
+def parse_monkeys(lines: list[str]) -> list[Monkey]:
+    return [parse_monkey(lines[i:i+6]) for i in range(0, len(lines) + 1, 7)]
+
+def parse_monkey(lines: list[str]) -> Monkey:
+    number = int(lines[0][-2])
+    items = [int(n) for n in lines[1].lstrip('  Starting items: ').split(', ')]
+    operation = [
+        lambda x: x*5,
+        lambda x: x+5,
+        lambda x: x*19,
+        lambda x: x+7,
+        lambda x: x+2,
+        lambda x: x+1,
+        lambda x: x*x,
+        lambda x: x+4,
+    ][number]
+    divisor = int(lines[3].split(' ')[-1])
+    target_true = int(lines[4].split(' ')[-1])
+    target_false = int(lines[5].split(' ')[-1])
+
+    return Monkey(items, operation, divisor, target_true, target_false)
+
+monkeys = parse_monkeys(read_lines())
+lcm = functools.reduce(lambda x, y: x*y, (m.divisor for m in monkeys))
+for monkey in monkeys:
+    monkey.set_worry_level_limiter(lcm)
+
+for round_nr in range(10000):
+    if not round_nr % 100:
+        print(round_nr)
+    for monkey in monkeys:
+        for _ in range(len(monkey.items)):
+            item, target = monkey.inspect()
+            monkeys[target].receive(item)
+
+sorted_monkeys = list(sorted(monkeys, key=lambda x: x.inspections_number(), reverse=True))
+print(sorted_monkeys[0].inspections_number(), sorted_monkeys[1].inspections_number())
+print(sorted_monkeys[0].inspections_number() * sorted_monkeys[1].inspections_number())

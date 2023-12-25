bc636ed878dee51e24dc2e2007421f2ef2934858
Wojciech Pedrak <woj123@gmail.com>
2023-12-25 21:30:24 +0100

    2021-23-1 done (in a legit way)

--- 2021/23/solution.py
+++ 2021/23/solution.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+
 ROOM_LENGTH = 2
+board_state = tuple[tuple[str,...], str, int]
 
 def get_lines(filename='input.txt') -> list[str]:
     return [line.rstrip() for line in open(filename, 'r', encoding='utf-8')]
@@ -16,9 +19,11 @@ def get_rooms(board: list[list[str]]) -> tuple[str]:
 def unit_cost(symbol: str) -> int:
     return {'A': 1, 'B': 10, 'C': 100, 'D': 1000}[symbol]
 
-def corridor_dist(src: int, dst: int):
-    regular_dist = dst - src + 1
-    rooms_in_between = sum(src <= x < dst for x in range(1, 5))
+def corridor_dist(src: int, dst: int) -> int:
+    left = min(src, dst)
+    right = max(src, dst)
+    regular_dist = right - left
+    rooms_in_between = sum(left <= x < right for x in range(1, 5))
     return regular_dist + rooms_in_between
 
 def one_step(corridor: str, room_number: int, symbol: str) -> list[tuple[str, int]]:
@@ -27,17 +32,66 @@ def one_step(corridor: str, room_number: int, symbol: str) -> list[tuple[str, in
     for idx in range(left, -1, -1):
         if corridor[idx] != '.':
             break
-        available_corridor_spots.append((idx, corridor_dist(idx, left)))
+        available_corridor_spots.append((idx, corridor_dist(idx, left) + 1))
 
     for idx in range(right, len(corridor)):
         if corridor[idx] != '.':
             break
-        available_corridor_spots.append((idx, corridor_dist(right, idx)))
+        available_corridor_spots.append((idx, corridor_dist(right, idx) + 1))
 
-    return [(corridor[:idx] + symbol + corridor[idx+1:], dist) for idx, dist in available_corridor_spots]
+    return [(corridor[:idx] + symbol + corridor[idx+1:], dist) for idx, dist in available_corridor_spots]    
 
-def short_circuit(rooms: tuple[str], corridor: str, cost: int):
-    pass
+def can_enter_room(symbol: str, room: str, room_idx: int) -> bool:
+    room_symbol = 'ABCD'[room_idx]
+    return symbol == room_symbol and all(c == room_symbol for c in room)
+
+def is_room_entry(corridor_idx: int, right: bool):
+    if right:
+        return 1 <= corridor_idx <= 4
+    return 2 <= corridor_idx <= 5
+
+def short_circuit_one_direction(state: board_state, right=True) -> board_state:
+    rooms, corridor, cost = state
+    last_symbol = '.'
+    symbol_idx = 0
+
+    corridor_with_index = list(enumerate(corridor))
+    if not right:
+        corridor_with_index= reversed(corridor_with_index)
+
+    for idx, symbol in corridor_with_index:
+        if symbol != '.':
+            last_symbol = symbol
+            symbol_idx = idx
+        if not is_room_entry(idx, right):
+            continue
+        room_idx = idx - 1 if right else idx - 2
+        room = rooms[room_idx]
+        if not can_enter_room(last_symbol, room, room_idx):
+            continue
+        corridor = corridor[:symbol_idx] + '.' + corridor[symbol_idx+1:]
+        
+        room = rooms[room_idx] + last_symbol
+        rooms = rooms[:room_idx] + (room,) + rooms[room_idx+1:]
+
+        corridor_distance = corridor_dist(idx, symbol_idx) + 1
+        room_distance = ROOM_LENGTH - len(room) + 1
+        cost += (corridor_distance + room_distance) * unit_cost(last_symbol)
+
+        last_symbol = '.'
+
+    return rooms, corridor, cost
+
+def short_circuit(state: board_state) -> board_state:
+    state_changed = True
+    while state_changed:
+        corridor_before = state[1]
+        state = short_circuit_one_direction(state, right=True)
+        state = short_circuit_one_direction(state, right=False)
+        corridor_after = state[1]
+        state_changed = corridor_before != corridor_after
+
+    return state
 
 def one_step_and_cleanup(rooms: tuple[str], corridor: str, cost: int):
     one_step_states = []
@@ -51,25 +105,61 @@ def one_step_and_cleanup(rooms: tuple[str], corridor: str, cost: int):
 
     return [short_circuit(s) for s in one_step_states]
 
+def print_history(prev: dict[board_state, board_state], state: board_state) -> None:
+    while state in prev:
+        print(state)
+        state = prev[state]
+    print(state)
+    print('')
+
 def min_cost(rooms: tuple[str]):
     to_visit = [(rooms, '.' * 7, 0)]
-    visited = set()
+    visited = defaultdict(lambda: 9876543210123456789)
     min_cost = 9876543210123456789
-
+    prev = {}
     while to_visit:
         rooms, corridor, cost = to_visit.pop()
-        if rooms == ('AA', 'BB', 'CC', 'DD'):
-            min_cost = min(min_cost, cost)
+        if rooms == tuple(x * ROOM_LENGTH for x in 'ABCD'):
+            if cost >= min_cost:
+                continue
+            min_cost =cost
+            print_history(prev, (rooms, corridor, cost))
+            
             continue
 
-        if rooms in visited:
+        if visited[(rooms, corridor)] <= cost:
             continue
 
-        visited.add(rooms)
-        to_visit += one_step_and_cleanup(rooms, corridor, cost)
+        visited[(rooms, corridor)] = cost
+        next_steps = one_step_and_cleanup(rooms, corridor, cost)
+        for step in next_steps:
+            prev[step] = (rooms, corridor, cost)
+        to_visit += next_steps
         
     return min_cost
 
+if True:
+    test1 = short_circuit_one_direction((('', '', '', ''), 'DB.....', 0), right=True)
+    assert test1[2] == 50
+    assert 'B' not in test1[1]
+    assert test1[0][1] == 'B'
+
+    test2 = short_circuit_one_direction((('', '', '', ''), 'DB.....', 0), right=False)
+    assert test2[1] == 'DB.....'
+
+    test3 = short_circuit((('', '', '', ''), 'DB.....', 0))
+    assert test3[1] == '.......'
+
+    test4 = short_circuit((('', '', '', ''), '.....CA', 0))
+    assert test4[1] == '.......'
+
+    test5 = one_step('..B....', 2, 'C')
+    assert ('..BC...', 1) in test5
+
+    test6 = one_step_and_cleanup(('BA', 'CD', 'C', 'DA'), '..B....', 40)
+    assert (('BA', 'D', 'CC', 'DA'), '..B....', 440) in test6
+
+    print('tests passed')
 
 board = get_board()
 rooms = get_rooms(board)

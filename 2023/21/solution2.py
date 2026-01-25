d4009200b2607ce8560b5efd92fc618deb519a62
Wojciech Pedrak <woj123@gmail.com>
2026-01-25 12:11:44 +0100

    2023-21-2 parity fixes, remove last cnt

--- 2023/21/solution2.py
+++ 2023/21/solution2.py
@@ -31,17 +31,21 @@ def achievable_count(plots: Plots, start: Point) -> list[int]:
     dist = 0
     cnt: list[int] = []
     to_visit = deque([start, 'UP'])
-    visited: set[Point] = set()
+    visited_even: set[Point] = set()
+    visited_odd: set[Point] = set()
+    visited = visited_even
     
     while to_visit:
         position = to_visit.popleft()
 
         if isinstance(position, str):
+            if not to_visit:
+                break
+
+            to_visit.append('UP')
             cnt.append(len(visited))
             dist += 1
-
-            if to_visit:
-                to_visit.append('UP')
+            visited = visited_even if is_even(dist) else visited_odd
             continue
 
         if position in visited:
@@ -94,15 +98,15 @@ def count_path(map_size: int, cnt: list[int], remaining_steps: int) -> int:
     full_map_cnt_even, full_map_steps_even = (cnt[-1], max_cnt) if is_even(max_cnt) else (cnt[-2], max_cnt-1)
     full_map_cnt_odd, full_map_steps_odd = (cnt[-1], max_cnt) if is_odd(max_cnt) else (cnt[-2], max_cnt-1)
 
-    first_map_parity = (STEPS - remaining_steps) % 2
+    first_map_is_even = is_even(remaining_steps)
 
     # go till you can reach every point in the map
     # 1. go straight
     full_maps = remaining_steps // map_size
     remaining_steps -= full_maps * map_size
     # 2. it might be that we weren't able to reach every point in the last map, so go back 1 map
-    pre_last_map_parity = (first_map_parity + full_maps - 1) % 2
-    pre_last_map_full_steps = full_map_steps_even if is_even(pre_last_map_parity) else full_map_steps_odd
+    pre_last_map_is_even = is_even(first_map_is_even + full_maps - 1)
+    pre_last_map_full_steps = full_map_steps_even if pre_last_map_is_even else full_map_steps_odd
     if remaining_steps + map_size < pre_last_map_full_steps:
         options_count += cnt[remaining_steps]  # count very last map before going back
         full_maps -= 1
@@ -111,7 +115,7 @@ def count_path(map_size: int, cnt: list[int], remaining_steps: int) -> int:
     # count the whole path
     options_count += full_maps//2 * full_map_cnt_even
     options_count += full_maps//2 * full_map_cnt_odd
-    first_map_cnt = full_map_cnt_even if is_even(first_map_parity) else full_map_cnt_odd
+    first_map_cnt = full_map_cnt_even if first_map_is_even else full_map_cnt_odd
     options_count += first_map_cnt if is_odd(full_maps) else 0
     # count the last map (possibly last AFTER taking step back)
     options_count += cnt[remaining_steps]
@@ -136,13 +140,18 @@ def run() -> None:
     assert '#' not in {row[start_position[0]] for row in plots[1:-1]}
     assert '#' not in set(plots[start_position[1]][1:-1])
 
-    result = max(cnt for dist, cnt in enumerate(achievable_count(plots, start_position)) if dist % 2 == STEPS % 2)
+    cnt = achievable_count(plots, start_position)
+    full_map_cnt_even = cnt[-1] if is_even(len(cnt)-1) else cnt[-2]
+    full_map_cnt_odd = cnt[-1] if is_odd(len(cnt)-1) else cnt[-2]
+
+    result = full_map_cnt_even if is_even(STEPS) else full_map_cnt_odd
     result += count_cross(plots)
     result += count_diagonals(plots)
 
     print(result)
     # so far failed with the following results
     assert 638110317012349 < result < 1278103017916873
+
 
 def experiment():
     plots = read_plots(filename="example.txt")
@@ -153,8 +162,10 @@ def experiment():
     cnt1 = achievable_count(plots, corner)
     cnt2 = achievable_count(plots, mid_top)
     
-    print(len(cnt1), cnt1[-5:])
-    print(len(cnt2), cnt2[-5:])
+    print(len(cnt1), cnt1[::2])
+    print(len(cnt1), cnt1[1::2])
+    print(len(cnt2), cnt2[::2])
+    print(len(cnt2), cnt2[1::2])
     print(sum(sum(x in '.S' for x in row) for row in plots))
 
 
@@ -168,5 +179,5 @@ def experiment():
 # In exactly 1000 steps, he can reach 668697 garden plots.
 # In exactly 5000 steps, he can reach 16733044 garden plots.
 
-# run()
-experiment()
+run()
+# experiment()

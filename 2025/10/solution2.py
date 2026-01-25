74225fd8ffd50b08922a081210c9c9fb1c3b6efe
Wojciech Pedrak <woj123@gmail.com>
2026-01-25 09:26:52 +0100

    2025-10-2 still working on it

--- 2025/10/solution2.py
+++ 2025/10/solution2.py
@@ -16,7 +16,7 @@ def read_input():
 
     return result
 
-def reduce(target: Counters, buttons: list[Button]) -> tuple[int, Counters, list[Button]]:
+def reduce_sigle_counter_coverage(target: Counters, buttons: list[Button]) -> tuple[int, Counters, list[Button]]:
     cnt = Counter(b for button in buttons for b in button)
     buttons = buttons[:]
     target_list = list(target)
@@ -46,20 +46,43 @@ def find_button(buttons: list[Button], key: int) -> tuple[int, Button]:
     
     raise Exception(':<')
 
+def max_count(target: Counters, button: Button):
+    return min(target[b] for b in button)
+
+def is_easy(target: Counters, buttons: list[Button]):
+    options = 1
+    for button in buttons:
+        options *= max_count(target, button)
+    return options <= 10**6
+
+def count_clicks_brute_force(target: Counters, buttons: list[Button]):
+    
+
 def count_clicks(target: Counters, buttons: list[Button]):
     return 0
 
 def run() -> None:
     result = 0
-    cnt = 0
-    for target, buttons in read_input():
-        clicks, target, buttons = reduce(target, buttons)
-        result += clicks
-        result += count_clicks(target, buttons)
-        if clicks:
-            cnt += 1
-            print(clicks, buttons, target)
+    easy, hard = 0, 0
+    missing = 0
 
-    print(result, cnt)
+    for target, buttons in read_input():
+        clicks, target, buttons = reduce_sigle_counter_coverage(target, buttons)
+        result += clicks
+        if not buttons:
+            continue
+        
+        if min(len(b) for b in buttons) == max(len(b) for b in buttons):
+            result += sum(target) // len(buttons[0])
+            continue
+
+        easy += is_easy(target, buttons)
+        hard += not is_easy(target, buttons)
+
+        # result += count_clicks(target, buttons)
+        missing += 1
+
+    print(f'missing: {missing}')
+    print(result, easy, hard)
 
 run()
